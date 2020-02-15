4923ec12499868613e9489e9706d8122fdccb7d0
Anthony Ioimo <anthonylioimo@gmail.com>
2020-02-16 00:54:05 +0100

    Player first check if out of bounds

--- js/players/Player.js
+++ js/players/Player.js
@@ -14,17 +14,21 @@ class Player {
 
   //determines point value of a given space
   evaluateCoordinate(y, x) {
-    const square = game.world[y][x];
-
     if (
       y < 0 ||
       x < 0 ||
       game.world.length - 1 < y ||
-      game.world.length - 1 < x ||
-      !square.passable
+      game.world.length - 1 < x
     ) {
       return -1;
     }
+
+    const square = game.world[y][x];
+
+    if (!square.passable) {
+      return -1;
+    }
+
     //Square does not belong to an opponent.
     if (this.checkIfBlank(square)) return 2;
     if (this.checkIfOwnColor(square)) return 1;
